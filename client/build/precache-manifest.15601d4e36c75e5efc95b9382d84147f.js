645f55bb814366853e9d1e13dbd07cdb5cbe6f6f
Kakashi <nickperel@live.com>
2022-03-23 18:23:29 -0400

    finished refactoring components to use Global State

---
+++ client/build/precache-manifest.15601d4e36c75e5efc95b9382d84147f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ae71ee617c9e7296f2cab5b73e4292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b45eaae6e3a39bca53f",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "186d877ee5436dfec1c2",
+    "url": "/static/js/2.ab80c57e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.ab80c57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b45eaae6e3a39bca53f",
+    "url": "/static/js/main.f5a4cac9.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
